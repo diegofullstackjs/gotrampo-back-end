9853ff30caf7deefd487aea0bce51ecb934d85ed
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-26 22:55:56 +0100

    habilitando bodyParser

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 const path = require('path')
 require('dotenv').config({path: path.resolve(__dirname,'src','config','.env')})
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const http = require('http')
 const express = require('express')
+const mongodb = require('./src/config/database')
 const indexRoutes = require('./src/routes/index')
 const usersRoutes = require('./src/routes/users')
 
@@ -12,7 +14,14 @@ const server = http.createServer(app)
 
 process.env.MODE === 'development'? app.use(morgan('dev')) : app.use(morgan('combined'))
 
-app.use(indexRoutes)
-app.use(usersRoutes)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use('/',indexRoutes)
+app.use('/users',usersRoutes)
+
+
+server.listen(process.env.PORT || 3333,() => {
+    mongodb()
+})
 const io = require('socket.io').listen(server)
-server.listen(process.env.PORT || 3333)
