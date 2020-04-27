c3d8e86b7242699f2f7f5ad482af7730360205d5
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-27 18:35:32 +0100

    Calcular distancia entre dois pontos com mongodb

---
+++ src/controllers/MainController.js
@@ -0,0 +1,24 @@
+const UserModel = require('../models/Usuario')
+
+module.exports.fetch = async (req,res) =>{
+    try{
+        console.log(req.body)
+        const find = await UserModel.find({
+            location:{
+                $near:{
+                    $maxDistance: 0,
+                    $maxDistance: 1000,
+                    $geometry: {
+                     type: "Point",
+                     coordinates: [req.body.longitude, req.body.latitude]
+                    }
+                }
+            }
+        }).exec()
+
+        return res.status(200).json(find)
+    }catch(e){
+        console.warn(e)
+        return res.status(500).json({error: true, message: "Ocorreu um erro inesperado"})
+    }
+}
